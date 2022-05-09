863fb56b6e76dbbdcd4ca170701810c71d0ea6c9
undid-iridium <francis.x.irizarry@gmail.com>
2022-05-09 14:16:37 -0400

    Add project files.

---
+++ TestFF/Config.cs
@@ -0,0 +1,17 @@
+﻿using Exiled.API.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFF
+{
+    /// <inheritdoc />
+    public class Config : IConfig
+    {
+
+        /// <inheritdoc />
+        public bool IsEnabled { get; set; } = true;
+    }
+}
---
+++ TestFF/Main.cs
@@ -0,0 +1,76 @@
+﻿using Exiled.API.Features;
+using Exiled.API.Features.DamageHandlers;
+using Exiled.Events.EventArgs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServerEvents = Exiled.Events.Handlers.Server;
+namespace TestFF
+{
+    public class Main : Plugin<Config>
+    {
+        public void RefreshBools()
+        {
+            ServerConfigSynchronizer serversync = new ServerConfigSynchronizer();
+            serversync.RefreshMainBools();
+        }
+
+        public void OnShooting(ShootingEventArgs ev)
+        {
+            ev.Shooter.SendConsoleMessage($"FriendlyFire Enabled : {Server.FriendlyFire}", "red");
+        }
+
+        public void OnRoundStarted()
+        {
+         
+            foreach (Player p in Player.List)
+            {
+                p.Broadcast(2, "Friendly Fire Disabled - RoundEnded", global::Broadcast.BroadcastFlags.Normal, true);
+            }
+            Server.FriendlyFire = false;
+   
+            RefreshBools();
+            PlayerStatsSystem.AttackerDamageHandler.RefreshConfigs();
+        }
+
+
+        public void OnEndingRound(RoundEndedEventArgs ev)
+        {
+            if (!Round.IsEnded)
+            {
+                return;
+            }
+
+            foreach (Player p in Player.List)
+            {
+                p.Broadcast(2, "Friendly Fire Enabled - RoundEnding", global::Broadcast.BroadcastFlags.Normal, true);
+            }
+            FriendlyFireConfig.PauseDetector = true;
+            Server.FriendlyFire = true;
+            RefreshBools();
+            PlayerStatsSystem.AttackerDamageHandler.RefreshConfigs();
+        }
+
+
+        /// <inheritdoc />
+        public override void OnEnabled()
+        {
+         
+            ServerEvents.RoundStarted += this.OnRoundStarted;
+            ServerEvents.RoundEnded += this.OnEndingRound;
+
+            base.OnEnabled();
+        }
+
+        /// <inheritdoc />
+        public override void OnDisabled()
+        {
+            ServerEvents.RoundStarted -= this.OnRoundStarted;
+            ServerEvents.RoundEnded -= this.OnEndingRound;
+            base.OnDisabled();
+        }
+
+    }
+}
